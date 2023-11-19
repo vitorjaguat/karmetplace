94475d4aed16a13f0fba95ad6162c7846d933ddf
Vitor Butkus <vitorbutkus@gmail.com>
2023-11-19 18:10:54 -0300

    set defaultTheme to dark in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -84,7 +84,7 @@ function AppWrapper(props: AppProps & { baseUrl: string }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="light"
+      defaultTheme="dark"
       value={{
         dark: darkTheme.className,
         light: 'light',
