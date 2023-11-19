6a0649ea26525a88257ffadc3740f9abb0c0f123
Vitor Butkus <vitorbutkus@gmail.com>
2023-11-19 18:01:03 -0300

    test

---
+++ components/home/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <div className="relative z-10 w-full h-fit px-32 pb-40 pt-32">
+      <div className=""></div>
+      <div className="">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis ipsum
+        sapiente corrupti iure? Debitis voluptatem perferendis unde molestiae
+        quia dolorum qui vitae, quod culpa nulla sapiente, explicabo facilis
+        non. Repellendus aliquam officia, pariatur delectus similique recusandae
+        praesentium sed facilis aspernatur nulla veniam enim id vero et eos
+        nihil cupiditate laboriosam voluptate? Sint a molestias quo, maxime
+        natus illo, eligendi praesentium velit non porro nam quisquam repellat
+        assumenda ea iure magni quidem dolor officiis fugit voluptas. Fugit
+        doloribus ipsam natus maiores earum rerum perferendis sunt ex dolorem
+        molestias hic nemo, aperiam a similique voluptatibus. Incidunt labore
+        suscipit quas repellendus blanditiis mollitia?
+      </div>
+    </div>
+  )
+}
