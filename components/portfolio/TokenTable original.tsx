2ae9fd4a6275cd168434884f64b887ac61ee0c16
Vitor Butkus <vitorbutkus@gmail.com>
2023-11-23 13:14:32 -0300

    add split modal in TokenTable. keep original TokenTable

--- components/portfolio/TokenTable original.tsx
+++ components/portfolio/TokenTable original.tsx
@@ -1,42 +1,11 @@
-import { ListModal, ListStep, useTokens } from '@reservoir0x/reservoir-kit-ui'
-import { Button } from 'components/primitives'
-import {
-  cloneElement,
-  ComponentProps,
-  ComponentPropsWithoutRef,
-  FC,
-  ReactNode,
-  useContext,
-} from 'react'
-import { CSS } from '@stitches/react'
-import { SWRResponse } from 'swr'
-import { useAccount, useWalletClient } from 'wagmi'
-import { useConnectModal } from '@rainbow-me/rainbowkit'
-import { ToastContext } from 'context/ToastContextProvider'
-import { useMarketplaceChain } from 'hooks'
-import { createPortal } from 'react-dom'
-
-import { Modal } from 'react-responsive-modal'
-
-// const orderFee = process.env.NEXT_PUBLIC_MARKETPLACE_FEE
-
-type ListingCurrencies = ComponentPropsWithoutRef<
-  typeof ListModal
->['currencies']
-
-type PropsList = {
-  token?: ReturnType<typeof useTokens>['data'][0]
-  buttonCss?: CSS
-  buttonChildren?: ReactNode
-  buttonProps?: ComponentProps<typeof Button>
-  mutate?: SWRResponse['mutate']
-}
-
 import {
   Dispatch,
+  FC,
   SetStateAction,
   useEffect,
   useRef,
+  useContext,
+  useState,
   forwardRef,
   useImperativeHandle,
   useMemo,
@@ -50,10 +19,11 @@ import {
   HeaderRow,
   Tooltip,
   FormatCryptoCurrency,
+  Button,
   Box,
   Grid,
 } from '../primitives'
-import { AcceptBid } from 'components/buttons'
+import { List, AcceptBid } from 'components/buttons'
 import Image from 'next/image'
 import { useIntersectionObserver } from 'usehooks-ts'
 import LoadingSpinner from '../common/LoadingSpinner'
@@ -61,6 +31,7 @@ import {
   EditListingModal,
   EditListingStep,
   useReservoirClient,
+  useTokens,
   useUserTokens,
 } from '@reservoir0x/reservoir-kit-ui'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -71,13 +42,11 @@ import {
   faGasPump,
   faMagnifyingGlass,
   faRefresh,
-  faHeart,
-  faAngleDoubleRight,
 } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
 import { MutatorCallback } from 'swr'
 import { Address } from 'wagmi'
-// import { useMarketplaceChain } from 'hooks'
+import { useMarketplaceChain } from 'hooks'
 import { NAVBAR_HEIGHT } from 'components/navbar'
 import Checkbox from 'components/primitives/Checkbox'
 import { UserToken } from 'pages/portfolio/[[...address]]'
@@ -85,7 +54,7 @@ import { ChainContext } from 'context/ChainContextProvider'
 import { Dropdown, DropdownMenuItem } from 'components/primitives/Dropdown'
 import { PortfolioSortingOption } from 'components/common/PortfolioSortDropdown'
 import CancelListing from 'components/buttons/CancelListing'
-// import { ToastContext } from 'context/ToastContextProvider'
+import { ToastContext } from 'context/ToastContextProvider'
 import fetcher from 'utils/fetcher'
 import { DATE_REGEX, timeTill } from 'utils/till'
 import { spin } from 'components/common/LoadingSpinner'
@@ -94,9 +63,6 @@ import { OpenSeaVerified } from 'components/common/OpenSeaVerified'
 import { ItemView } from './ViewToggle'
 import PortfolioTokenCard from './PortfolioTokenCard'
 import optimizeImage from 'utils/optimizeImage'
-import { useState } from 'react'
-import { SizeProp } from '@fortawesome/fontawesome-svg-core'
-import { spanStatusfromHttpCode } from '@sentry/nextjs'
 
 type Props = {
   address: Address | undefined
@@ -169,6 +135,7 @@ export const TokenTable = forwardRef<TokenTableRef, Props>(
       revalidateOnMount: true,
       fallbackData: [],
     })
+    console.log(tokens)
 
     useEffect(() => {
       mutate()
@@ -333,9 +300,6 @@ const TokenTableRow: FC<TokenTableRowProps> = ({
   const [isRefreshing, setIsRefreshing] = useState(false)
   const isSmallDevice = useMediaQuery({ maxWidth: 900 })
 
-  const [openHeart, setOpenHeart] = useState(false)
-  const [heartValue, setHeartValue] = useState(0)
-
   const addSelectedItem = (item: UserToken) => {
     setSelectedItems([...selectedItems, item])
   }
@@ -376,126 +340,6 @@ const TokenTableRow: FC<TokenTableRowProps> = ({
   const contract = token.token?.collection?.id
     ? token.token?.collection.id?.split(':')[0]
     : undefined
-
-  const List: FC<PropsList> = ({
-    token,
-    buttonCss,
-    buttonChildren,
-    buttonProps,
-    mutate,
-  }) => {
-    const { isDisconnected } = useAccount()
-    const { openConnectModal } = useConnectModal()
-    const { addToast } = useContext(ToastContext)
-
-    const marketplaceChain = useMarketplaceChain()
-    // console.log(marketplaceChain)
-
-    const { data: signer } = useWalletClient()
-    // console.log(signer)
-    const listingCurrencies: ListingCurrencies =
-      marketplaceChain.listingCurrencies
-    const tokenId = token?.token?.tokenId
-    const contract = token?.token?.contract
-
-    // console.log(openConnectModal)
-
-    const trigger = (
-      <Button css={buttonCss} color="primary" {...buttonProps}>
-        {buttonChildren}
-      </Button>
-    )
-
-    //connecting orderFee to heartValue:
-    const [orderFee, setOrderFee] = useState('')
-    const [orderFees, setOrderFees] = useState<string[]>([])
-    useEffect(() => {
-      const newOrderFee =
-        '0x5C6DC3b2a55be4b02e26b75848e27c19df4Af9fE:' + heartValue * 100
-      setOrderFee(newOrderFee)
-      setOrderFees([newOrderFee])
-    }, [heartValue])
-
-    // const orderFees = orderFee ? [orderFee] : []
-    // console.log(heartValue)
-
-    //list heart useEffect
-    const heartPortalRef = useRef()
-    const HeartModal = ({}) => {
-      return (
-        <div className="sticky top-0 left-0 h-screen w-screen bg-black/30 flex items-center justify-center z-[1000000000] text-black">
-          <div className="flex flex-col items-center justify-center p-4 w-1/2 h-1/2 bg-white ">
-            <div className="">
-              Você é bonzinho e quer doar uma porcentagem para o projeto?
-            </div>
-            <form>
-              <input
-                type="number"
-                name="percentage"
-                id="percentage"
-                placeholder="0"
-                defaultValue="0"
-                onChange={(e) => setHeartValue(+e.target.value)}
-              />
-              <button
-                onClick={() => {
-                  setOpenHeart(false)
-                  // +open real List!
-                }}
-              >
-                ok
-              </button>
-            </form>
-          </div>
-        </div>
-      )
-    }
-    useEffect(() => {
-      if (openHeart) {
-        createPortal(<HeartModal />, document.body, 'heart')
-      }
-    }, [openHeart])
-    // console.log(openHeart)
-
-    if (isDisconnected) {
-      return cloneElement(trigger, {
-        onClick: async () => {
-          if (!signer) {
-            openConnectModal?.()
-          }
-        },
-      })
-    } else
-      return (
-        <ListModal
-          trigger={trigger}
-          // nativeOnly={true}
-          collectionId={contract}
-          tokenId={tokenId}
-          feesBps={orderFees}
-          enableOnChainRoyalties={true}
-          currencies={listingCurrencies}
-          chainId={marketplaceChain.id}
-          onClose={(data, stepData, currentStep) => {
-            if (mutate && currentStep == ListStep.Complete) mutate()
-            setOpenHeart(false)
-          }}
-          onListingError={(err: any) => {
-            if (err?.code === 4001) {
-              addToast?.({
-                title: 'User canceled transaction',
-                description: 'You have canceled the transaction.',
-              })
-              return
-            }
-            addToast?.({
-              title: 'Could not list token',
-              description: 'The transaction was not completed.',
-            })
-          }}
-        />
-      )
-  }
 
   if (isSmallDevice) {
     return (
@@ -612,121 +456,23 @@ const TokenTableRow: FC<TokenTableRowProps> = ({
             />
           ) : null}
           {isOwner ? (
-            <>
-              {/* 2 step listing with custom fee: (MOBILE) */}
-              <div
-                className="px-5 py-1 bg-neutral-700 font-bold rounded-lg flex items-center cursor-pointer"
-                onClick={() => setOpenHeart(true)}
-              >
-                List
-              </div>
-
-              {/* {openHeart && (
-                
-              )} */}
-              <Modal
-                open={openHeart}
-                onClose={() => setOpenHeart(false)}
-                classNames={{
-                  modal: 'customModal',
-                  // modalContainer: 'customModal',
-                }}
-                center
-              >
-                <div className="flex flex-col items-center justify-center px-6 pt-16 pb-8 gap-4 bg-slate-600 ">
-                  <div className="flex flex-col gap-3">
-                    <div className="">
-                      Would you like to set a percentage of your sale to be
-                      donated to{' '}
-                      <span className="font-bold">
-                        The Sphere Common Treasury
-                      </span>{' '}
-                      multisig, so we can continue funding live art?
-                    </div>
-                    <div className="">
-                      If YES, select the percentage using the slider, then click
-                      "Next". The donation will be deducted from the total price
-                      you set.
-                    </div>
-                    <div className="">
-                      If NOT, that's ok - you can click "Next" and proceed to
-                      your listing.
-                    </div>
-                  </div>
-                  <form className="mt-4 flex flex-col gap-6">
-                    <div className="text-center bg-white/20 rounded-lg p-6 pt-10 px-10 pb-10 flex flex-col gap-6 items-center">
-                      <div className="text-center text-neutral-300 flex">
-                        Your donation:{' '}
-                        <div className=" font-bold w-[3rem]">{heartValue}%</div>
-                      </div>
-                      <FontAwesomeIcon
-                        icon={faHeart}
-                        className="duration-300 ease-in-out"
-                        color="purple"
-                        size={
-                          ((heartValue + 10 - (heartValue % 10)) / 10 +
-                            'x') as SizeProp
-                        }
-                      />
-
-                      <input
-                        type="range"
-                        name="percentage"
-                        id="percentage"
-                        placeholder="0"
-                        defaultValue="0"
-                        max={
-                          token?.token?.chainId == 1 ||
-                          token?.token?.chainId == 5
-                            ? 90
-                            : 80
-                        }
-                        className="w-[260px]"
-                        onChange={(e) => setHeartValue(+e.target.value)}
-                      />
-                    </div>
-                    <div className="mt-6 flex gap-4 justify-around">
-                      <button
-                        className="py-2 px-3 rounded-lg bg-[#2c2c59]"
-                        onClick={() => {
-                          setOpenHeart(false)
-                        }}
-                      >
-                        Cancel
-                      </button>
-                      {/* regular List button: */}
-                      <List
-                        token={token as ReturnType<typeof useTokens>['data'][0]}
-                        mutate={mutate}
-                        buttonCss={{
-                          width: '100%',
-                          maxWidth: '300px',
-                          justifyContent: 'center',
-                          px: '20px',
-                          backgroundColor: '$gray9',
-                          border: '1px solid white',
-                          color: '$gray12',
-                          '&:hover': {
-                            backgroundColor: '$gray4',
-                          },
-                        }}
-                        buttonChildren={
-                          <span className="flexjustify-betweenw-full">
-                            <FontAwesomeIcon
-                              className="mr-3"
-                              icon={faAngleDoubleRight}
-                            />
-                            Next
-                          </span>
-                        }
-                      />
-                    </div>
-                  </form>
-                </div>
-              </Modal>
-            </>
+            <List
+              token={token as ReturnType<typeof useTokens>['data'][0]}
+              mutate={mutate}
+              buttonCss={{
+                width: '100%',
+                maxWidth: '300px',
+                justifyContent: 'center',
+                px: '20px',
+                backgroundColor: '$gray3',
+                color: '$gray12',
+                '&:hover': {
+                  backgroundColor: '$gray4',
+                },
+              }}
+              buttonChildren="List"
+            />
           ) : null}
-
           <Dropdown
             modal={false}
             trigger={
@@ -1146,116 +892,19 @@ const TokenTableRow: FC<TokenTableRowProps> = ({
               />
             ) : null}
 
-            <>
-              {/* 2 step listing with custom fee: (medium) */}
-              <div
-                className="px-5 py-1 bg-neutral-700 font-bold rounded-lg flex items-center cursor-pointer"
-                onClick={() => setOpenHeart(true)}
-              >
-                List
-              </div>
-
-              {/* {openHeart && (
-                
-              )} */}
-              <Modal
-                open={openHeart}
-                onClose={() => setOpenHeart(false)}
-                center
-              >
-                <div className="flex flex-col items-center justify-center px-6 pt-16 pb-4 gap-4 bg-slate-600 ">
-                  <div className="flex flex-col gap-3">
-                    <div className="">
-                      Would you like to set a percentage of your sale to be
-                      donated to{' '}
-                      <span className="font-bold">
-                        The Sphere Common Treasury
-                      </span>{' '}
-                      multisig, so we can continue funding live art?
-                    </div>
-                    <div className="">
-                      If YES, select the percentage using the slider, then click
-                      "Next". The donation will be deducted from the total price
-                      you set.
-                    </div>
-                    <div className="">
-                      If NOT, that's ok - you can click "Next" and proceed to
-                      your listing.
-                    </div>
-                  </div>
-                  <form className="mt-4 flex flex-col gap-6 w-full items-center">
-                    <div className="text-center gap-8 bg-white/20 rounded-lg p-6 pt-10 px-10 w-fit">
-                      <input
-                        type="range"
-                        name="percentage"
-                        id="percentage"
-                        placeholder="0"
-                        defaultValue="0"
-                        max={
-                          token?.token?.chainId == 1 ||
-                          token?.token?.chainId == 5
-                            ? 90
-                            : 80
-                        }
-                        className="w-[300px] mb-6"
-                        onChange={(e) => setHeartValue(+e.target.value)}
-                      />
-                      <div className="text-center pb-5 text-neutral-300 flex justify-center">
-                        Your donation:{' '}
-                        <div className="ml-4 font-bold min-w-[3rem]">
-                          {heartValue}%
-                        </div>
-                      </div>
-                      <FontAwesomeIcon
-                        icon={faHeart}
-                        color="purple"
-                        className="duration-300 ease-in-out"
-                        size={
-                          ((heartValue + 10 - (heartValue % 10)) / 10 +
-                            'x') as SizeProp
-                        }
-                      />
-                    </div>
-                    <div className="flex gap-4 w-full justify-between mt-4">
-                      <button
-                        className="py-2 px-5 rounded-lg bg-[#2c2c59]"
-                        onClick={() => {
-                          setOpenHeart(false)
-                        }}
-                      >
-                        Cancel
-                      </button>
-
-                      {/* regular List button: */}
-                      <List
-                        token={token as ReturnType<typeof useTokens>['data'][0]}
-                        buttonCss={{
-                          px: '100px',
-                          backgroundColor: '$gray3',
-                          color: '$gray12',
-                          border: '1px solid white',
-                          '&:hover': {
-                            backgroundColor: '$gray4',
-                          },
-                        }}
-                        // buttonChildren="List"
-                        buttonChildren={
-                          <span className="flexjustify-betweenw-full">
-                            <FontAwesomeIcon
-                              className="mr-3"
-                              icon={faAngleDoubleRight}
-                            />
-                            Next
-                          </span>
-                        }
-                        mutate={mutate}
-                      />
-                    </div>
-                  </form>
-                </div>
-              </Modal>
-            </>
-
+            <List
+              token={token as ReturnType<typeof useTokens>['data'][0]}
+              buttonCss={{
+                px: '42px',
+                backgroundColor: '$gray3',
+                color: '$gray12',
+                '&:hover': {
+                  backgroundColor: '$gray4',
+                },
+              }}
+              buttonChildren="List"
+              mutate={mutate}
+            />
             <Dropdown
               modal={false}
               trigger={
