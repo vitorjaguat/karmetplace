bbf18f71a06074a5e1f6cc013907c449ce9006be
Vitor Butkus <vitorbutkus@gmail.com>
2023-11-24 18:43:48 -0300

    update markups in faq page

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -41,24 +41,24 @@ const AboutPage: NextPage = () => {
       {
         title: 'How can I collect the works minted as non-fungible tokens?',
         content:
-          "To collect the works, you will need a non-custodial wallet that has a balance corresponding to the price of the works in Ether, the native cryptocurrency of the Ethereum blockchain. If you don't have Ether, you can buy it on an centralized exchange like https://coinbase.com, https://bitstamp.com or in a decentralized exchange like https://app.uniswap.org",
+          "To collect the works, you will need a non-custodial wallet that has a balance corresponding to the price of the works in Ether, the native cryptocurrency of the Ethereum blockchain. If you don't have Ether, you can buy it on an centralized exchange like <a class='underline text-neutral-300' href='https://coinbase.com' target='_blank' rel='noopener noreferrer'>https://coinbase.com</a>, <a class='underline text-neutral-300' href='https://bitstamp.com' target='_blank' rel='noopener noreferrer'>https://bitstamp.com</a> or in a decentralized exchange like <a class='underline text-neutral-300' href='https://app.uniswap.org' target='_blank' rel='noopener noreferrer'>https://app.uniswap.org</a>.",
       },
       {
         title:
           'How can I install a non-custodial wallet? Do I need to pay for one?',
         content:
-          'You can install Metamask through the website https://metamask.io, for free.',
+          "You can install Metamask through the website <a class='underline text-neutral-300' href='https://metamask.io' target='_blank' rel='noopener noreferrer'>https://metamask.io</a>, for free.",
       },
       {
         title: 'What is a non-custodial wallet? Is it safe?',
         content:
-          'A non-custodial wallet is a digital cryptocurrency wallet that is installed in your internet browser as an extension (add-on), in which the custody of the assets is not centralized by any third party entity (like a bank, for instance), but by the person who owns the wallet itself. It is recommended to use Metamask, the most widely used wallet. When installing Metamask in your browser, you will be asked to securely note down a secret recovery phrase (seed phrase) of 12 words that gives you access to the wallet. It is very important to have a backup of this phrase, preferably physically and never share it with anyone, because with this phrase you (or anyone in possession of it) can access the wallet on any device. As a browser extension, Metamask has proven to be quite secure, requiring a minimum amount of personal information sharing. For more details on its operation, visit https://metamask.io/faqs/.',
+          "A non-custodial wallet is a digital cryptocurrency wallet that is installed in your internet browser as an extension (add-on), in which the custody of the assets is not centralized by any third party entity (like a bank, for instance), but by the person who owns the wallet itself. It is recommended to use Metamask, the most widely used wallet. When installing Metamask in your browser, you will be asked to securely note down a secret recovery phrase (seed phrase) of 12 words that gives you access to the wallet. <span class='font-bold'>It is very important to have a backup of this phrase, preferably physically and never share it with anyone, because with this phrase you (or anyone in possession of it) can access the wallet on any device.</span> As a browser extension, Metamask has proven to be quite secure, requiring a minimum amount of personal information sharing. For more details on its operation, visit <a class='underline text-neutral-300' href='https://metamask.io/faqs/' target='_blank' rel='noopener noreferrer'>https://metamask.io/faqs/</a>.",
       },
       {
         title:
           'Is it necessary to use the MetaMask wallet? Can I use other wallets?',
         content:
-          'You can use other wallets that operate on the Ethereum blockchain to connect to the Karmetplace. We offer support to Rainbow, Coinbase Wallet, and Wallet Connect, besides MetaMask, but we recommend you to use MetaMask. It is compatible with many decentralized applications, and also with most internet browsers. It is considered safe because it is frequently updated in rigorous audit processes. If you are interested in discovering other wallets that are compatible with the EVM (Ethereum Virtual Machine), visit https://ethereum.org/en/wallets/find-wallet/.',
+          "You can use other wallets that operate on the Ethereum blockchain to connect to the Karmetplace. We offer support to Rainbow, Coinbase Wallet, and Wallet Connect, besides MetaMask, but we recommend you to use MetaMask. It is compatible with many decentralized applications, and also with most internet browsers. It is considered safe because it is frequently updated in rigorous audit processes. If you are interested in discovering other wallets that are compatible with the EVM (Ethereum Virtual Machine), visit <a class='underline text-neutral-300' href='https://ethereum.org/en/wallets/find-wallet/' target='_blank' rel='noopener noreferrer'>https://ethereum.org/en/wallets/find-wallet/</a>.",
       },
       {
         title: 'Which collections are available on the Karmetplace?',
@@ -75,17 +75,17 @@ const AboutPage: NextPage = () => {
         title:
           'Can I collect NFTs from The Anarchiving Game contract for free?',
         content:
-          'Yes. Please go to the corresponding section on https://thesphere.as to find which tokens are available to be collected for free, plus a small gas fee on Zora mainnet.',
+          "Yes. Please go to the corresponding section on <a class='underline text-neutral-300' href='https://thesphere.as' target='_blank' rel='noopener noreferrer'>https://thesphere.as</a> to find which tokens are available to be collected for free, plus a small gas fee on Zora mainnet.",
       },
       {
         title: 'How can I change the network I’m connected to?',
         content:
-          'The user interface might prompt you to a network change if you need to interact with a network that your wallet is not connected to. If you want to manually add other networks, you can go to https://chainlist.org/ and add them to your wallet’s network selector area.',
+          "The user interface might prompt you to a network change if you need to interact with a network that your wallet is not connected to. If you want to manually add other networks, you can go to <a class='underline text-neutral-300' href='https://chainlist.org/' target='_blank' rel='noopener noreferrer'>https://chainlist.org/</a> and add them to your wallet’s network selector area.",
       },
       {
         title: 'What are gas fees, and how are they determined?',
         content:
-          'Gas fees are payments made by users to compensate for the computing energy required to process and validate transactions on the Ethereum blockchain. They vary based on network demand and transaction complexity. On Zora mainnet, these fees are considerably smaller. On Ethereum mainnet the fees can be a bit higher, but the contract we deployed have optimal gas optimization settings. You can check status of gas fees prices on https://etherscan.io/gastracker.',
+          "Gas fees are payments made by users to compensate for the computing energy required to process and validate transactions on the Ethereum blockchain. They vary based on network demand and transaction complexity. On Zora mainnet, these fees are considerably smaller. On Ethereum mainnet the fees can be a bit higher, but the contract we deployed have optimal gas optimization settings. You can check status of gas fees prices on <a class='underline text-neutral-300' href='https://etherscan.io/gastracker' target='_blank' rel='noopener noreferrer'>https://etherscan.io/gastracker</a>.",
       },
       {
         title: 'How does onchain royalty enforcement work?',
@@ -105,13 +105,13 @@ const AboutPage: NextPage = () => {
       {
         title: 'Does the Karmetplace host special events or exhibitions?',
         content:
-          'Yes, we host various community events, immersive virtual exhibitions, live presentations, lectures, and encounters. You can connect with us though Twitter, Telegram, Discord, and Substack to know about our past and upcoming events.',
+          "Yes, we host various community events, immersive virtual exhibitions, live presentations, lectures, and encounters. You can connect with us through <a class='underline text-neutral-300' href='https://twitter.com/TheSphere_as' target='_blank' rel='noopener noreferrer'>Twitter</a>, <a class='underline text-neutral-300' href='https://t.me/+o3hn1fgGsQMzZjgx' target='_blank' rel='noopener noreferrer'>Telegram</a>, <a class='underline text-neutral-300' href='https://discord.com/invite/e8K8KPrJ49' target='_blank' rel='noopener noreferrer'>Discord</a>, and <a class='underline text-neutral-300' href='https://thesphere.substack.com/' target='_blank' rel='noopener noreferrer'>Substack</a> to know about our past and upcoming events.",
       },
       {
         title:
           'I have other questions that were not addressed in this manual. What should I do?',
         content:
-          'You reach out on our Telegram channel and ask for support there.',
+          "You reach out on our <a class='underline text-neutral-300' href='https://t.me/+o3hn1fgGsQMzZjgx' target='_blank' rel='noopener noreferrer'>Telegram</a> channel and ask for support there.",
       },
       // {
       //   title: t('faq.q13'),
@@ -123,6 +123,7 @@ const AboutPage: NextPage = () => {
   return (
     <Layout>
       <Head />
+
       <Flex
         direction="column"
         align="center"
@@ -165,7 +166,7 @@ const AboutPage: NextPage = () => {
               collaborative spirit of art creation.
             </div>
           </div>
-          <div className="py-6 mt-20 mb-20">
+          <div className="py-6 mt-20 mb-20 text-left">
             <Faq data={data} styles={styles} config={config} />
           </div>
         </div>
