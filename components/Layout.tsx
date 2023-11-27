fc93409e4e3dbee1c97c08f1fd284bb20c37571f
Vitor Butkus <vitorbutkus@gmail.com>
2023-11-27 13:33:09 -0300

    conditionally showing currencies depending on the seleceted chain in Wallet

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,12 +2,14 @@ import { Box } from 'components/primitives'
 import { FC, ReactNode } from 'react'
 import Navbar from './navbar'
 import { Footer } from './home/Footer'
+import { useRouter } from 'next/router'
 
 type Props = {
   children: ReactNode
 }
 
 const Layout: FC<Props> = ({ children }) => {
+  const router = useRouter()
   return (
     <>
       <Box
@@ -21,7 +23,7 @@ const Layout: FC<Props> = ({ children }) => {
         <Box css={{ maxWidth: 4500, mx: 'auto' }}>
           <Navbar />
           <main>{children}</main>
-          <Footer />
+          {!router.pathname.includes('/portfolio') && <Footer />}
         </Box>
         <div className="site-background">
           <div className="star-container">
--- components/navbar/Wallet.tsx
+++ components/navbar/Wallet.tsx
@@ -7,15 +7,16 @@ import {
   FormatCurrency,
   Text,
 } from 'components/primitives'
-import { mainnet, polygon, optimism } from 'wagmi/chains'
+import { mainnet, polygon, optimism, zora, goerli } from 'wagmi/chains'
 import { useAccount, useContractReads, erc20ABI, useBalance } from 'wagmi'
-import { useMemo, useState } from 'react'
+import { useMemo, useState, useContext, useEffect } from 'react'
 import { zeroAddress, formatUnits } from 'viem'
 import { useCoinConversion } from '@reservoir0x/reservoir-kit-ui'
+import { ChainContext } from 'context/ChainContextProvider'
 
 //CONFIGURABLE: Here you may configure currencies that you want to display in the wallet menu. Native currencies,
 //like ETH/MATIC etc need to be fetched in a different way. Configure them below
-const currencies = [
+const allCurrencies = [
   {
     address: zeroAddress,
     symbol: 'ETH',
@@ -37,61 +38,175 @@ const currencies = [
     coinGeckoId: 'weth',
   },
   {
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    symbol: 'USDC',
+    decimals: mainnet.nativeCurrency.decimals,
+    chain: {
+      id: mainnet.id,
+      name: mainnet.name,
+    },
+    coinGeckoId: 'usd-coin',
+  },
+  {
+    address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+    symbol: 'USDT',
+    decimals: mainnet.nativeCurrency.decimals,
+    chain: {
+      id: mainnet.id,
+      name: mainnet.name,
+    },
+    coinGeckoId: 'tether',
+  },
+  {
     address: zeroAddress,
-    symbol: 'MATIC',
-    decimals: polygon.nativeCurrency.decimals,
+    symbol: 'ETH',
+    decimals: zora.nativeCurrency.decimals,
     chain: {
-      id: polygon.id,
-      name: polygon.name,
+      id: zora.id,
+      name: zora.name,
     },
-    coinGeckoId: 'matic-network',
+    coinGeckoId: 'ethereum',
   },
   {
-    address: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+    address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     symbol: 'WETH',
-    decimals: polygon.nativeCurrency.decimals,
+    decimals: zora.nativeCurrency.decimals,
     chain: {
-      id: polygon.id,
-      name: polygon.name,
+      id: zora.id,
+      name: zora.name,
     },
     coinGeckoId: 'weth',
   },
   {
-    address: '0x4200000000000000000000000000000000000006',
-    symbol: 'WETH',
-    decimals: optimism.nativeCurrency.decimals,
+    address: zeroAddress,
+    symbol: 'ETH',
+    decimals: goerli.nativeCurrency.decimals,
     chain: {
-      id: optimism.id,
-      name: optimism.name,
+      id: goerli.id,
+      name: goerli.name,
+    },
+    coinGeckoId: 'ethereum',
+  },
+  {
+    address: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+    symbol: 'WETH',
+    decimals: goerli.nativeCurrency.decimals,
+    chain: {
+      id: goerli.id,
+      name: goerli.name,
     },
     coinGeckoId: 'weth',
   },
+  // {
+  //   address: zeroAddress,
+  //   symbol: 'MATIC',
+  //   decimals: polygon.nativeCurrency.decimals,
+  //   chain: {
+  //     id: polygon.id,
+  //     name: polygon.name,
+  //   },
+  //   coinGeckoId: 'matic-network',
+  // },
+  // {
+  //   address: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+  //   symbol: 'WETH',
+  //   decimals: polygon.nativeCurrency.decimals,
+  //   chain: {
+  //     id: polygon.id,
+  //     name: polygon.name,
+  //   },
+  //   coinGeckoId: 'weth',
+  // },
+  // {
+  //   address: '0x4200000000000000000000000000000000000006',
+  //   symbol: 'WETH',
+  //   decimals: optimism.nativeCurrency.decimals,
+  //   chain: {
+  //     id: optimism.id,
+  //     name: optimism.name,
+  //   },
+  //   coinGeckoId: 'weth',
+  // },
 ]
 
-type EnhancedCurrency = (typeof currencies)[0] & {
+type EnhancedCurrency = (typeof allCurrencies)[0] & {
   usdPrice: number
   balance: string | number | bigint
 }
 
-const nonNativeCurrencies = currencies.filter(
-  (currency) => currency.address !== zeroAddress
-)
+type Currency = {
+  address: string
+  symbol: string
+  decimals: number
+  chain: {
+    id: number
+    name: string
+  }
+  coinGeckoId: string
+}
 
-const currencySymbols = currencies.map((currency) => currency.symbol).join(',')
-const currencyCoingeckoIds = currencies
-  .map((currency) => currency.coinGeckoId)
-  .join(',')
+// const nonNativeCurrencies = currencies.filter(
+//   (currency) => currency.address !== zeroAddress
+// )
+
+// const currencySymbols = currencies.map((currency) => currency.symbol).join(',')
+// const currencyCoingeckoIds = currencies
+//   .map((currency) => currency.coinGeckoId)
+//   .join(',')
 
 const Wallet = () => {
+  const { chain } = useContext(ChainContext)
   const [viewAll, setViewAll] = useState(false)
   const { address } = useAccount()
+  const [currencies, setCurrencies] = useState<any[]>([])
+  const [nonNativeCurrencies, setNonNativeCurrencies] = useState<Currency[]>([])
+  const [currencySymbols, setCurrencySymbols] = useState<string>('')
+  const [currencyCoingeckoIds, setCurrencyCoingeckoIds] = useState<string>('')
+
+  // let nonNativeCurrencies: Currency[]
+  // let currencySymbols: any
+  // let currencyCoingeckoIds: any
+
+  useEffect(() => {
+    let allCurrs: Currency[] = allCurrencies
+    let currs: Currency[]
+    if (chain.id === 1) {
+      currs = allCurrs.filter(
+        (currency: Currency) => currency.chain.id === mainnet.id
+      )
+    } else if (chain.id === 7777777) {
+      currs = allCurrs.filter(
+        (currency: Currency) => currency.chain.id === zora.id
+      )
+    } else {
+      currs = allCurrs.filter(
+        (currency: Currency) => currency.chain.id === goerli.id
+      )
+    }
+    setCurrencies(currs)
+    const nonNativeCurrs = currs.filter(
+      (currency: Currency) => currency.address !== zeroAddress
+    )
+    setNonNativeCurrencies(nonNativeCurrs)
+
+    const currencySymbs = currs
+      .map((currency: Currency) => currency.symbol)
+      .join(',')
+    setCurrencySymbols(currencySymbs)
+
+    const currencyCoingeckoIdss = currs
+      .map((currency: Currency) => currency.coinGeckoId)
+      .join(',')
+    setCurrencyCoingeckoIds(currencyCoingeckoIdss)
+  }, [chain.name])
+
   const { data: nonNativeBalances } = useContractReads({
     contracts: nonNativeCurrencies.map((currency) => ({
       abi: erc20ABI,
       address: currency.address as `0x${string}`,
       chainId: currency.chain.id,
       functionName: 'balanceOf',
-      args: [address as any],
+      args: [address as string],
     })),
     watch: true,
     enabled: address ? true : false,
@@ -104,10 +219,14 @@ const Wallet = () => {
     address,
     chainId: mainnet.id,
   })
-  const maticBalance = useBalance({
+  const zoraBalance = useBalance({
     address,
-    chainId: polygon.id,
+    chainId: zora.id,
   })
+  // const maticBalance = useBalance({
+  //   address,
+  //   chainId: polygon.id,
+  // })
 
   const usdConversions = useCoinConversion(
     'USD',
@@ -127,12 +246,16 @@ const Wallet = () => {
       if (currency.address === zeroAddress) {
         //CONFIGURABLE: Configure these to show the fetched balance results configured above in the useBalance hooks
         switch (currency.chain.id) {
-          case polygon.id: {
-            balance = maticBalance.data?.value || 0n
-            break
-          }
+          // case polygon.id: {
+          //   balance = maticBalance.data?.value || 0n
+          //   break
+          // }
           case mainnet.id: {
             balance = ethBalance.data?.value || 0n
+            break
+          }
+          case zora.id: {
+            balance = zoraBalance.data?.value || 0n
             break
           }
         }
@@ -169,7 +292,7 @@ const Wallet = () => {
       }
     }) as EnhancedCurrency[]
     //CONFIGURABLE: Configure these to regenerate whenever a native balance changes, non native balances are already handled
-  }, [usdConversions, nonNativeBalances, ethBalance, maticBalance])
+  }, [usdConversions, nonNativeBalances, ethBalance, zoraBalance])
 
   const totalUsdBalance = useMemo(() => {
     return enhancedCurrencies.reduce(
